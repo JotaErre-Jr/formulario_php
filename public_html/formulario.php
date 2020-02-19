a5c2dcd17683ea6554ed5b73027c0dc9729c5f46
Jotaerre <rochajotaerre@gmail.com>
2020-02-19 12:56:12 -0300

    formulario HTML concluido, falta alicar css e php

---
+++ public_html/formulario.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>FORMULARIO DE CADASTRO</title>
+  </head>
+  <body>
+    <h1>CADASTRO REALIZADO COM SUCESSO</h1>
+  </body>
+</html>
